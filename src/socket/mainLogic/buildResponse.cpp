de5cd783abc1293ce2b1ef4706bb2ba6f90731f2
anirin <atu8823atu@gmail.com>
2025-03-25 15:58:20 +0900

    connection refactor

---
+++ src/socket/mainLogic/buildResponse.cpp
@@ -0,0 +1,61 @@
+#include "Connection.hpp"
+
+// todo 命名変更
+void Connection::setErrorFd(int status_code) {
+	if(conf_value_._error_page.empty() ||
+	   (!conf_value_._error_page.empty() &&
+		conf_value_._error_page.find(status_code) == conf_value_._error_page.end())) {
+		std::map<std::string, std::string> status_code_phrase;
+
+		status_code_phrase["301"] = "Moved Permanently";
+		status_code_phrase["302"] = "Found";
+		status_code_phrase["400"] = "Bad Request";
+		status_code_phrase["404"] = "Not Found";
+		status_code_phrase["405"] = "Method Not Allowed";
+		status_code_phrase["413"] = "Content Too Long";
+		status_code_phrase["500"] = "Internal Server Error";
+		status_code_phrase["504"] = "Gateway Timeout";
+		status_code_phrase["505"] = "HTTP Version Not Supported";
+
+		std::stringstream ss;
+		ss << status_code;
+		std::string status_code_str = ss.str();
+
+		std::cerr << "[connection] error_page is not set" << std::endl;
+		wbuff_ = "<html>\r\n"
+				 "<head><title>" +
+				 status_code_phrase[status_code_str] +
+				 "</title></head>\r\n"
+				 "<body>\r\n"
+				 "<h1>" +
+				 status_code_str + " " + status_code_phrase[status_code_str] +
+				 "</h1>\r\n"
+				 "</body>\r\n"
+				 "</html>\r\n";
+
+		return;
+	}
+
+	// 本来は内部リダイレクトによって更新処理をおこなう
+	// ヘッダーを書き換えて 再帰的にprocess after read
+	// completedを呼び出すと思われる（無限ループに陥らないように注意する） error page は絶対パスで指定されることにする
+	std::string error_page = conf_value_._error_page[status_code];
+	std::cout << "[connection] error_page: " << error_page << " is set" << std::endl;
+	readStaticFile(error_page);
+
+	return;
+}
+
+void Connection::buildStaticFileResponse(int status_code) {
+	std::map<std::string, std::string> r_header = request_->getHeader();
+	std::string path = request_->getLocationPath();
+	std::string server_name = request_->getServerName();
+
+	response_ = new HttpResponse();
+	response_->setBody(wbuff_);
+	response_->setStartLine(status_code);
+	response_->setHeader(r_header, path, server_name);
+
+	wbuff_ = response_->buildResponse();
+	std::cout << "[connection] response build" << std::endl;
+}
---
+++ src/socket/mainLogic/redirect.cpp
@@ -0,0 +1,19 @@
+#include "Connection.hpp"
+
+// redirectの処理の場合
+// todo error ハンドリング
+FileStatus Connection::buildRedirectResponse(int status_code, std::string redirect_path) {
+	std::cout << "[connection] building redirect response to: " << redirect_path << std::endl;
+
+	std::map<std::string, std::string> r_header = request_->getHeader();
+
+	response_ = new HttpResponse();
+	setErrorFd(status_code); // error ではないが同じロジック
+	response_->setBody(wbuff_);
+	response_->setStartLine(status_code);
+	r_header["Location"] = redirect_path;
+	response_->setHeader(r_header, "", request_->getServerName());
+
+	wbuff_ = response_->buildResponse();
+	return SUCCESS_STATIC;
+}
